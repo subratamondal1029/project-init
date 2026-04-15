55d31c7af89fdaf49aadeeb4cb9c2a2a154cef0c
Subrata Mondal <subratamondal@tutanota.com>
2026-04-15 20:10:08 +0530

    feat(tool): add logger

---
+++ src/utils/logger.ts
@@ -0,0 +1,10 @@
+import { createConsola } from "consola";
+
+export const logger = createConsola({
+  level: 3, // 0 silent → 5 verbose
+  formatOptions: {
+    compact: true,
+    colors: true,
+    date: false
+  }
+});
