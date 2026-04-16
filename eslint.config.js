fa0e6df4d459cb0bcbff92d14fe533ab6bf21841
Subrata Mondal <subratamondal@tutanota.com>
2026-04-16 20:44:33 +0530

    build(config): ignore template files in eslint and tsconfig

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ export default [
   js.configs.recommended,
   ...tseslint.configs.recommended,
   {
-    ignores: ["dist/**/*", "node_modules/**/*"],
+    ignores: ["src/templates/**/*", "dist/**/*", "node_modules/**/*"],
   },
   {
     files: ["src/**/*.ts"],
