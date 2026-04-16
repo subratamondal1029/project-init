36bb59f334cb14e386cc287fc29b685ba593d41a
Subrata Mondal <subratamondal@tutanota.com>
2026-04-16 12:53:05 +0530

    refactor(state): refactor sharedState and tsState to use class-based structure

--- src/state/shared.state.ts
+++ src/state/shared.state.ts
@@ -1,9 +1,23 @@
-import type { SharedState } from "@/types/state.type.js";
+import { AVAILABLE_LANGUAGE } from "@/constants.js";
+import type { SharedState as State } from "@/types/state.type.js";
 
 // store shared questions answers with default value
-export const sharedState: SharedState = {
-  language: "TS",
-  projectName: "my-project",
-  git: true,
-  gitOrigin: "https://github.com/username/repo.git",
-};
+class SharedState implements State {
+  public language: (typeof AVAILABLE_LANGUAGE)[number]["code"];
+  public projectName: string;
+  public git: boolean;
+  public gitOrigin?: string;
+
+  constructor() {
+    this.language = "TS";
+    this.projectName = "my-project";
+    this.git = true;
+    this.gitOrigin = "https://github.com/username/repo.git";
+  }
+
+  setValues(answers: Partial<State>) {
+    Object.assign(this, answers);
+  }
+}
+
+export const sharedState = new SharedState();
--- src/state/ts.state.ts
+++ src/state/ts.state.ts
@@ -1,13 +1,30 @@
-import type { TsState } from "@/types/state.type.js";
+import type { TsState as State, TsPackageManagers } from "@/types/state.type.js";
 
-// typescript project answer state with default value
-export const tsState: TsState = {
-  packageManager: "pnpm",
-  eslint: true,
-  prettier: true,
-  lintStaged: true,
-  husky: true,
-  preCommit: "pnpm run lint:stage",
-  commitLint: true,
-  installDeps: true,
-};
+// store typescript questions answers with default value
+class TsState implements State {
+  public packageManager: TsPackageManagers;
+  public eslint: boolean;
+  public prettier: boolean;
+  public lintStaged: boolean;
+  public husky: boolean;
+  public preCommit: string;
+  public commitLint: boolean;
+  public installDeps: boolean;
+
+  constructor() {
+    this.packageManager = "pnpm";
+    this.eslint = true;
+    this.prettier = true;
+    this.lintStaged = true;
+    this.husky = true;
+    this.preCommit = "pnpm run lint:stage";
+    this.commitLint = true;
+    this.installDeps = true;
+  }
+
+  setValues(answers: Partial<State>) {
+    Object.assign(this, answers);
+  }
+}
+
+export const tsState = new TsState();
