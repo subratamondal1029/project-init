e0b77ddbee0c8010d24eb5cfb931fc18eb0384a9
Subrata Mondal <subratamondal@tutanota.com>
2026-04-18 20:23:55 +0530

    feat(ts): add TypeScript initialization function to set up project files

--- src/core/ts/index.ts
+++ src/core/ts/index.ts
@@ -1,9 +1,11 @@
 import { logger } from "@/utils/logger.js";
 import { packageInit } from "./packageInit.js";
+import { tsInit } from "./tsInit.js";
 
 export const tsProjectInit = async (): Promise<void> => {
   try {
     await packageInit();
+    await tsInit();
   } catch (error) {
     logger.error("Error occurred while initializing TypeScript project");
     logger.error.raw(error);
---
+++ src/core/ts/tsInit.ts
@@ -0,0 +1,18 @@
+import { logger } from "@/utils/logger.js";
+import fs from "fs-extra";
+import path from "node:path";
+
+/*
+STEPS:
+1. copy tsconfig.json
+2. copy src/index.ts
+*/
+
+export const tsInit = async (): Promise<void> => {
+  // Implementation for initializing TypeScript project
+  logger.info("Initializing TypeScript project...");
+  await fs.copy("@/templates/ts/tsconfig.json", path.join(process.cwd(), "tsconfig.json"));
+  logger.info("tsconfig.json created.");
+  await fs.copy("@/templates/ts/src/index.ts", path.join(process.cwd(), "src/index.ts"));
+  logger.info("src/index.ts created.");
+};
