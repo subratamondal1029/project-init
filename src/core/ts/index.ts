54198bf28f36687f2474df2a7bf49575a36cde66
Subrata Mondal <subratamondal@tutanota.com>
2026-04-18 21:49:37 +0530

    feat(tsProjectInit): add initialization process with ESLint, Prettier, and Husky support

--- src/core/ts/index.ts
+++ src/core/ts/index.ts
@@ -1,11 +1,41 @@
 import { logger } from "@/utils/logger.js";
 import { packageInit } from "./packageInit.js";
 import { tsInit } from "./tsInit.js";
+import { tsState } from "@/state/ts.state.js";
+import { eslintInit } from "./eslintInit.js";
+import { prettierInit } from "./prettierInit.js";
+import { huskyInit } from "./huskyInit.js";
+import { sharedState } from "@/state/shared.state.js";
+import { filterDependencies, installPackages } from "./installPackages.js";
 
 export const tsProjectInit = async (): Promise<void> => {
   try {
     await packageInit();
     await tsInit();
+
+    if (tsState.eslint) {
+      await eslintInit();
+    }
+
+    if (tsState.prettier) {
+      await prettierInit();
+    }
+
+    if (sharedState.git && tsState.husky) {
+      await huskyInit();
+    }
+
+    if (tsState.installDeps) {
+      await installPackages();
+    } else {
+      logger.info("Skipping package installation.");
+      const dependencies = filterDependencies();
+      logger.box(
+        `To install packages later, run \`${tsState.packageManager} install ${dependencies.join(` `)}\``
+      );
+    }
+
+    logger.info("Everything is set up! Now you can start coding.");
   } catch (error) {
     logger.error("Error occurred while initializing TypeScript project");
     logger.error.raw(error);
