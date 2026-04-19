96e4274a5d202ce350899b0f1904af1c7455887e
Subrata Mondal <subratamondal@tutanota.com>
2026-04-19 18:37:39 +0530

    feat(template): add script to copy raw templates from src to dist

---
+++ scripts/copyTemplates.mjs
@@ -0,0 +1,10 @@
+import fs from "fs-extra"
+import path from "node:path"
+import { fileURLToPath } from "node:url"
+
+const _dirname = path.dirname(fileURLToPath(import.meta.url))
+
+const templatePath = path.resolve(_dirname, "../src/templates")
+const destinationPath = path.resolve(_dirname, "../dist/templates")
+
+fs.copySync(templatePath, destinationPath);
