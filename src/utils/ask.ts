3fbb3eae3bda985c9eb2780f4d320d320aea1dea
Subrata Mondal <subratamondal@tutanota.com>
2026-04-20 19:42:29 +0530

    fix(ask): update question processing to include custom result handling

--- src/utils/ask.ts
+++ src/utils/ask.ts
@@ -1,6 +1,12 @@
 import Enquirer, { Prompt } from "enquirer";
 
+function result(this: { skipped?: boolean; type: string }, value: string) {
+  return (this.type === "confirm" && this.skipped ? false : value) as unknown as string;
+}
+
 export const ask = async (questions: NonNullable<ConstructorParameters<typeof Prompt>[0]>[]) => {
+  const processedQuestions = questions.map((question) => ({ ...question, result }));
+
   const enquirer = new Enquirer();
-  return await enquirer.prompt(questions);
+  return await enquirer.prompt(processedQuestions);
 };
